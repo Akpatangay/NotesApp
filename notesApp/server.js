7371ebd35bc5ff964d622d3a071a717fe181415a
ankip <ankita.patangay@avantsy.com>
2020-02-09 21:20:47 +0530

    added meta tag

--- notesApp/server.js
+++ notesApp/server.js
@@ -9,18 +9,18 @@ var app = express();
 
 app.use(cors());
 
-const forceSSL = function() {
-  return function(req, res, next) {
-    if (req.headers["x-forwarded-proto"] !== "https") {
-      return res.redirect(["https://", req.get("Host"), req.url].join(""));
-    }
-    next();
-  };
-};
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
+// const forceSSL = function() {
+//   return function(req, res, next) {
+//     if (req.headers["x-forwarded-proto"] !== "https") {
+//       return res.redirect(["https://", req.get("Host"), req.url].join(""));
+//     }
+//     next();
+//   };
+// };
+// // Instruct the app
+// // to use the forceSSL
+// // middleware
+// app.use(forceSSL());
 
 //open app
 app.use(express.static(path.join(__dirname, "/dist/notesApp")));
